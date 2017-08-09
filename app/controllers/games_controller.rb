2a967fed329543ea70c63e41ee0a4e9e94e76414
Rachel Peace <email": null>
2017-08-09 15:18:00 -0500

    Add index and show for games

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,19 @@
+class GamesController < ApplicationController
+
+  def index
+    @games = Game.all
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+end
