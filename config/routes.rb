f5adb786de50c2e26d65bcd3a2be5aeb01b75daa
Katy McCloskey <email": null>
2017-08-09 17:15:02 -0500

    add styles, routes, links, etc to all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :games
+  resources :users, only: [:show]
   get '/' => "games#index"
-  get '/users/:id' => "users#show"
+
+
+ # get '/users/:id' => "users#show"
 end
