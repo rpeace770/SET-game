96d64675a98fcf08deb240143e67c59ba8c7d582
Rachel Peace <email": null>
2017-08-10 16:55:41 -0500

    fix css on board display

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809193746) do
+ActiveRecord::Schema.define(version: 20170810212500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "color"
+    t.string   "fill"
+    t.string   "number"
+    t.string   "shape"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.integer  "sets"
