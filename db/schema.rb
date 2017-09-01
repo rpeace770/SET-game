048ae57ef4ca5bef42fa30ca9a7b9a86383d295d
Rachel Peace <email": null>
2017-09-01 13:58:35 -0500

    update image links

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170811161740) do
     t.integer  "user_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.index ["user_id"], name: "index_games_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
