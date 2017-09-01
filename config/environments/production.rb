4f4f73c474eed3f845e5f7a0e9882fc77f223305
Rachel Peace <email": null>
2017-09-01 15:14:52 -0500

    Add config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,10 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # following two settings were not originally in this file
+  config.serve_static_assets = true
+  config.assets.digest = true
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -23,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
